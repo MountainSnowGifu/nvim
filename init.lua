4a3807b54f3184d7c435e6d8dc1ac93fef30b216
Akira <akira@example.com>
2026-06-08 22:38:22 +0900

    color

--- init.lua
+++ init.lua
@@ -397,10 +397,14 @@ do
     },
   }
 
+  vim.pack.add { gh 'EdenEast/nightfox.nvim' } -- lazy
+
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+
+  -- vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'carbonfox'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
