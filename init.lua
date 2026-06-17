936b97abe1a1ca392102b2ee6dffd6ffd06a5d54
MountainSnowGifu <mountain.snow.gifu@gmail.com>
2026-06-17 22:17:52 +0900

    test

--- init.lua
+++ init.lua
@@ -988,7 +988,7 @@ do
   vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
 
   -- Ensure basic parsers are installed
-  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'rust' }
   require('nvim-treesitter').install(parsers)
 
   ---@param buf integer
